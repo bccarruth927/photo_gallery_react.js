ce18d9f447be27fa0914ad2d1311bef353a939c5
bccarruth927 <brecarr1997@aol.com>
2023-05-16 12:17:01 -0400

    Add replicated content and styling to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
+//Import application stylesheet
 import './App.css';
+//Import images object
+import { images } from './modules/object.js';
+
+//import antelopes from './imgs/antelopes.jpg';
 
 function App() {
   return (
-    
+    <section id="wrapper">
+      <div class="container">
+        <h1>Amazing Animal Photos</h1>
+      </div>
+      <figure id="thumbnails">
+        <img src={images.antelopes.src} width="19.5%" height="19.5%" alt={images.antelopes.alt}/>
+        <img src={images.dolphins.src} width="19.5%" height="19.5%" alt={images.dolphins.alt}/>
+        <img src={images.elephants.src} width="19.5%" height="19.5%" alt={images.elephants.alt}/>
+        <img src={images.horses.src} width="19.5%" height="19.5%" alt={images.elephants.alt}/>
+        <img src={images.tiger.src} width="19.5%" height="19.5%" alt={images.tiger.alt}/>
+      </figure>
+      <figure id="largeImage">
+        <img src={images.antelopes.src} alt={images.antelopes.alt}/>
+        <figcaption class="caption">
+          {images.antelopes.caption}
+        </figcaption>
+      </figure>
+    </section>
   );
 }
 
---
+++ src/modules/object.js
@@ -0,0 +1,32 @@
+//JavaScript Document containing the images object used in the application
+
+//Import images from the imgs folder for the object
+import antelopes from '../imgs/antelopes.jpg';
+import dolphins from '../imgs/dolphins.jpg';
+import elephants from '../imgs/elephants.jpg';
+import horses from '../imgs/horses.jpg';
+import tiger from '../imgs/tiger.jpg';
+
+export const images = {
+    antelopes: {
+        src: antelopes,
+        alt: 'antelopes',
+        caption: 'Two antelopes resting under the shade of a small tree. Photo credit: Bas van Brandwijk from Pexels.com'
+    }, 
+    dolphins: {
+        src: dolphins,
+        alt: 'dolphins'
+    },
+    elephants: {
+        src: elephants,
+        alt: 'elephants'
+    },
+    horses: {
+        src: horses,
+        alt: 'horses'
+    },
+    tiger: {
+        src: tiger,
+        alt: 'tiger'
+    }
+};
